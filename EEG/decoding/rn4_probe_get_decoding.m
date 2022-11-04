2e8e902db93876e2c9c6fad320fa35ae8828c7e6
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-11-04 14:36:59 +0100

    Remove trials with wrong direction dial turn

--- EEG/decoding/rn4_probe_get_decoding.m
+++ EEG/decoding/rn4_probe_get_decoding.m
@@ -4,7 +4,7 @@ clc; clear; close all
 
 %% Define parameters
 
-subjects = 21:25;
+subjects = 1;
 
 for this_subject = subjects
     %% Parameters
@@ -27,6 +27,9 @@ for this_subject = subjects
     remove_RT = log.goodBadTrials(sub_logindex);
     good_RT = contains(remove_RT, 'TRUE');
 
+    wrong_dir = sub_log.wrongDir;
+    good_dir = contains(wrong_dir, 'FALSE');
+    
     %% Check if trials missing
     
     while length(good_RT) ~= length(trl2keep)
@@ -34,25 +37,33 @@ for this_subject = subjects
         for i = 1:length(trl2keep)
             if data.trialinfo(i) ~= sub_log.probeTrig(i)
                 good_RT(i) = [];
+                wrong_dir(i) = [];
                 break
             end
         end
 
     end
 
-    %% Remove bad trials
+    %% Keep good trials
     
     cfg = [];
-    cfg.trials = trl2keep & good_RT;
+    cfg.trials = trl2keep & good_RT & good_dir;
 
     data = ft_selectdata(cfg, data);
-    
+
     %% Remove bad ICA components
 
     cfg = [];
     cfg.component = ica2rem;
 
     data = ft_rejectcomponent(cfg, ica, data);
+
+    %% Surface laplacian
+
+    cfg = [];
+    cfg.elec = ft_read_sens('standard_1020.elc');
+
+    data = ft_scalpcurrentdensity(cfg, data);
 
     %% Baseline correction
 
@@ -61,49 +72,21 @@ for this_subject = subjects
     cfg.baselinewindow = [-.25 0];
 
     data = ft_preprocessing(cfg, data);
-    
-    %% Band selection
-
-    % Band selection
-    cfg = [];
-    cfg.bpfilter = 'yes';
-    cfg.hilbert = 'yes';
-
-    % Beta
-    cfg.bpfreq  = param.betaband;
-    data_beta   = ft_preprocessing(cfg, data);
-
-    % Alpha
-    cfg.bpfreq  = param.alphaband;
-    data_alpha  = ft_preprocessing(cfg, data);
 
     %% Resample
 
     cfg = [];
-    cfg.resamplefs = 50; 
+    cfg.resamplefs = 100; 
     
     data        = ft_resampledata(cfg, data);
-    data_beta   = ft_resampledata(cfg, data_beta);
-    data_alpha  = ft_resampledata(cfg, data_alpha);
-
-    %% Temporal smoothing (removing the higher frequencies)
-
-    cfg = [];
-    cfg.boxcar = 0.05; % smooth with 50 ms boxcar window
-    
-    data        = ft_preprocessing(cfg, data);
-    data_beta   = ft_preprocessing(cfg, data_beta);
-    data_alpha  = ft_preprocessing(cfg, data_alpha);
 
     %% Select data 
     
     cfg = [];
-    cfg.latency = [-.1 4]; % encoding window
+    cfg.latency = [-.1 2]; % encoding window
     cfg.channel = 'EEG'; % only keep EEG electrodes
     
     data        = ft_selectdata(cfg, data);
-    data_beta   = ft_selectdata(cfg, data_beta);
-    data_alpha  = ft_selectdata(cfg, data_alpha);
 
     %% Separate trial types
     
@@ -114,6 +97,10 @@ for this_subject = subjects
     % Target location
     trials_item_left     = ismember(data.trialinfo(:,1), param.triggers_item_left);
     trials_item_right    = ismember(data.trialinfo(:,1), param.triggers_item_right);
+
+%     % Target tilt
+%     trials_tilt_left     =                              
+%     trials_tilt_right
     
     % Load
     trials_load_two      = ismember(data.trialinfo(:,1), param.triggers_load_two);
@@ -126,27 +113,21 @@ for this_subject = subjects
 
     % Load one - T1
     cfg.trials = trials_load_two;
-
-    data_two         = ft_timelockanalysis(cfg, data);                     % put all trials into a single matrix
-    data_two_beta    = ft_timelockanalysis(cfg, data_beta);
-    data_two_alpha   = ft_timelockanalysis(cfg, data_alpha);
+    data_two   = ft_timelockanalysis(cfg, data);
 
     % Load one - T2
     cfg.trials = trials_load_four;
-    
-    data_four         = ft_timelockanalysis(cfg, data);
-    data_four_beta    = ft_timelockanalysis(cfg, data_beta);
-    data_four_alpha   = ft_timelockanalysis(cfg, data_alpha);
+    data_four  = ft_timelockanalysis(cfg, data);
 
     %% Decoding 
 
-    dtime = data.time;                                                        % Time variable
+    dtime       = data.time;
 
     %% Load two    
     
     % Data-sets
-    d = data_two.trial; d_beta = data_two_beta.trial; d_alpha = data_two_alpha.trial;   % data, and for beta, alpha
-    allTrials   = 1:size(data_two.trial, 1);                                            % Trials 
+    d           = data_two.trial;    
+    allTrials   = 1:size(data_two.trial, 1);
     
     % Classes
     motorClass  = trials_load_two & trials_resp_right; motorClass  = motorClass(trials_load_two);  % (1 for right)
@@ -155,15 +136,12 @@ for this_subject = subjects
     % Run decoding
     [decoding.motor_correct_two, decoding.motor_distance_two]                 = eeg_decoding(d, allTrials, motorClass, dtime);
     [decoding.visual_correct_two, decoding.visual_distance_two]               = eeg_decoding(d, allTrials, visualClass, dtime);
-    [decoding.motor_beta_correct_two, decoding.motor_beta_distance_two]       = eeg_decoding(d_beta, allTrials, motorClass, dtime);
-    [decoding.motor_alpha_correct_two, decoding.motor_alpha_distance_two]     = eeg_decoding(d_alpha, allTrials, motorClass, dtime);    
-    [decoding.visual_alpha_correct_two, decoding.visual_alpha_distance_two]   = eeg_decoding(d_alpha, allTrials, visualClass, dtime);
 
     %% Load four    
     
     % Data-sets
-    d = data_four.trial; d_beta = data_four_beta.trial; d_alpha = data_four_alpha.trial;   % data, and for beta, alpha
-    allTrials   = 1:size(data_four.trial, 1);                                              % Trials 
+    d           = data_four.trial;
+    allTrials   = 1:size(data_four.trial, 1);
     
     % Classes
     motorClass  = trials_load_four & trials_resp_right; motorClass  = motorClass(trials_load_four);  % (1 for right)
@@ -172,9 +150,6 @@ for this_subject = subjects
     % Run decoding
     [decoding.motor_correct_four, decoding.motor_distance_four]               = eeg_decoding(d, allTrials, motorClass, dtime);
     [decoding.visual_correct_four, decoding.visual_distance_four]             = eeg_decoding(d, allTrials, visualClass, dtime);
-    [decoding.motor_beta_correct_four, decoding.motor_beta_distance_four]     = eeg_decoding(d_beta, allTrials, motorClass, dtime);
-    [decoding.motor_alpha_correct_four, decoding.motor_alpha_distance_four]   = eeg_decoding(d_alpha, allTrials, motorClass, dtime);       
-    [decoding.visual_alpha_correct_four, decoding.visual_alpha_distance_four] = eeg_decoding(d_alpha, allTrials, visualClass, dtime);
 
     %% Add time variable
 
@@ -185,7 +160,6 @@ for this_subject = subjects
     save([param.path, 'Processed/Locked probe/decoding/' 'decoding_s' num2str(this_subject)], 'decoding');
     
 end
-
 
 %% Decoding general function
 
--- EEG/rn4_gen_param.m
+++ EEG/rn4_gen_param.m
@@ -41,25 +41,42 @@ function [param, eegfiles] = rn4_gen_param(this_subject)
 
     %% Triggers
 
-    param.triggers_probe        = [trials + 16, trials + 16 + 50, trials + 16 + 100, trials + 16 + 150];
+    param.triggers_probe        = trig_define(trials, [16,66,116,166]);
 
-    param.triggers_load_two     = [trials, trials + 16, trials + 32, trials + 100, trials + 16 + 100, trials + 32 + 100];    
-    param.triggers_load_four    = [trials + 50, trials + 16 + 50, trials + 32 + 50, trials + 150, trials + 16 + 150, trials + 32 + 150];
+    param.triggers_load_two     = trig_define(trials, [0,16,32,100,116,132]);
+    param.triggers_load_four    = param.triggers_load_two + 50;
 
-    param.triggers_dial_up      = [trials, trials + 16, trials + 32, trials + 50, trials + 16 + 50, trials + 32 + 50];
-    param.triggers_dial_right   = [trials + 100, trials + 16 + 100, trials + 32 + 100, trials + 150, trials + 16 + 150, trials + 32 + 150];
+    param.triggers_dial_up      = trig_define(trials, [0,16,32,50,66,82]);
+    param.triggers_dial_right   = param.triggers_dial_up + 100;
 
-    param.triggers_item_left    = [item_left, item_left + 16, item_left + 32, item_left + 50, item_left + 50 + 16, item_left + 50 + 32, item_left + 100, item_left + 100 + 16, item_left + 100 + 32, item_left + 150, item_left + 150 + 16, item_left + 150 + 32];
-    param.triggers_item_right   = [item_right, item_right + 16, item_right + 32, item_right + 50, item_right + 50 + 16, item_right + 50 + 32, item_right + 100, item_right + 100 + 16, item_right + 100 + 32, item_right + 150, item_right + 150 + 16, item_right + 150 + 32];
+    param.triggers_item_left    = trig_define(item_left, [0,16,32,50,66,82,100,116,132,150,166,182]);
+    param.triggers_item_right   = trig_define(item_right, [0,16,32,50,66,82,100,116,132,150,166,182]);
 
-    param.triggers_resp_left    = [tilt_left, tilt_left + 16, tilt_left + 32, tilt_left + 50, tilt_left + 50 + 16, tilt_left + 50 + 32, tilt_right + 100, tilt_right + 100 + 16, tilt_right + 100 + 32, tilt_right + 150, tilt_right + 150 + 16, tilt_right + 150 + 32];
-    param.triggers_resp_right   = [tilt_right, tilt_right + 16, tilt_right + 32, tilt_right + 50, tilt_right + 50 + 16, tilt_right + 50 + 32, tilt_left + 100, tilt_left + 100 + 16, tilt_left + 100 + 32, tilt_left + 150, tilt_left + 150 + 16, tilt_left + 150 + 32];
-    
+    param.triggers_tilt_left    = trig_define(tilt_left, [0,16,32,50,66,82,100,116,132,150,166,182]);
+    param.triggers_tilt_right   = trig_define(tilt_right, [0,16,32,50,66,82,100,116,132,150,166,182]);
+
+    param.triggers_resp_left    = [trig_define(tilt_left, [0,16,32,50,66,82]), trig_define(tilt_right, [100,116,132,150,166,182])];
+    param.triggers_resp_right   = [trig_define(tilt_right, [0,16,32,50,66,82]), trig_define(tilt_left, [100,116,132,150,166,182])];
+
     %% Bad channels
 
     param.chanrepsubs       = {     '6',                   '7',                  '10',                  '12',                          '14',                  '15',                  '17',                                    '21'};
     param.badchan           = {    'B25',             {'B25','B31'},             'B25',             {'A30','B25'},                 {'A30','B25'},             'B25',             {'B25','B31'},                     {'B10','B11','B12'}};
     param.replacechan       = {{'A31','B26'}, {{'A31','B26'},{'A30','B30'}}, {'A31','B26'}, {{'A29','A31'},{'A31','B26'}}, {{'A29','A31'},{'A31','B26'}}, {'A31','B26'}, {{'A31','B26'},{'A30','B30'}}, {{'B3','B9'},{'B9','B20'},{'B9','B13','B19'}}};
+
+    %% General function of adding condition codes to trial triggers
+    
+    function trig = trig_define(trials, factor)
+        
+        trig = [];
+        
+        for f = factor
+            trig = [trig, trials + f]; 
+        end
+
+    end
+
+end 
 
 
 
