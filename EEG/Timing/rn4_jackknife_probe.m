6ee187a9aa354b5ad41887432851b0886072cf7c
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-11-30 16:12:05 +0100

    Jackknife with increased TFR/decoding time resolution

--- EEG/Timing/rn4_jackknife_probe.m
+++ EEG/Timing/rn4_jackknife_probe.m
@@ -10,6 +10,12 @@ load([param.path, 'Processed/Locked probe/tfr contrasts probe/' 'cvsi_probe_all'
 load([param.path, 'Processed/Locked probe/tfr contrasts probe/' 'mean_cvsi_probe_all'], 'mean_cvsi_probe_all');
 load([param.path, 'Processed/Locked probe/decoding/' 'decoding_all'], 'decoding_all');
 
+%% General variables
+
+probe_titles = {'Motor - load two', 'Motor - load four', 'Visual - load two', 'Visual - load four'};
+subs = 1:25;
+peakperc = {'25%', '50%', '75%','100%'};
+
 %% TFR to timecourses
 
 fn = fieldnames(cvsi_probe_all);
@@ -31,13 +37,11 @@ end
 
 timecourses.time = timecourses.time(time_index);
 
-probe_titles = {'Motor - load two', 'Motor - load four', 'Visual - load two', 'Visual - load four'};
-subs = 1:25;
-peakperc = {'100% peak', '50% peak'};
-
 %% Jackknife, leave one out
 
-tpeak = zeros(4,2,25);
+peaks = 0.25:0.25:0.75; peak = length(peaks)+1;
+
+tpeak = zeros(4,peak,25);
 
 for s = 1:25
     
@@ -47,21 +51,27 @@ for s = 1:25
     i = 1;
 
     for f = 1:length(fn)
-        tc = mean(timecourses.(fn{f})(jsubs,:),1);
-        tpeak(i,1,s) = timecourses.time(find(tc == min(tc)));
-
-        ipeak = find(timecourses.time == tpeak(i,1,s));
-
-
-        for t = find(timecourses.time >= 0 & timecourses.time <= tpeak(i,1,s))
-            tc(t)
-            if tc(t) <= tc(ipeak)*0.50
-                tpeak(i,2,s) = timecourses.time(t);
-                break
-            end
     
-        end 
+        tc = mean(timecourses.(fn{f})(jsubs,:),1);
+        tpeak(i,peak,s) = timecourses.time(find(tc == min(tc)));
+
+        ipeak = find(timecourses.time == tpeak(i,peak,s));
+
+        for p = 1:length(peaks)
+
+            for t = find(timecourses.time >= 0 & timecourses.time <= tpeak(i,peak,s))
+
+                if tc(t) <= tc(ipeak)*peaks(p)
+                    tpeak(i,p,s) = timecourses.time(t);
+                    break
+                end
+        
+            end 
+
+        end
+
         i = i+1;
+
     end
 
 end
@@ -70,8 +80,6 @@ mean_tpeak = mean(tpeak,3);
 se_tpeak = std(tpeak,[],3)/sqrt(size(tpeak,3));
 
 %% Plot
-
-ylims = {[0.55 .95], [0.3 .65]};
 
 figure;
 
@@ -87,11 +95,84 @@ for i = 1:length(peakperc)
     
     ylabel('Peak time (s)'); xlabel('Condition'); title(peakperc{i})
     set(gca,'XtickLabel', probe_titles);
-    ylim(ylims{i})
 
 end
 
-%% Peak moment (on grand average)
+%% Decoding
+
+fn = fieldnames(decoding_all);
+fn = fn(contains(fn, 'correct'));
+fn = fn(~contains(fn, 'tilt'));
+
+time_index     = decoding_all.time >= -100 & decoding_all.time <= 1500;
+
+for f = 1:length(fn)
+    decoding_all.(fn{f}) = decoding_all.(fn{f})(:,time_index);
+end
+
+
+%% Jackknife, leave one out
+
+peaks = 0.25:0.25:0.75; peak = length(peaks)+1;
+tpeak = zeros(4,peak,25);
+
+for s = 1:25
+    
+    jsubs = subs;
+    jsubs(s) = [];
+    
+    i = 1;
+
+    for f = [1,3,2,4]
+    
+        tc = mean(decoding_all.(fn{f})(jsubs,:),1);
+        tpeak(i,peak,s) = decoding_all.time(find(tc == max(tc)));
+        ipeak = find(decoding_all.time == tpeak(i,peak,s));
+
+        for p = 1:length(peaks)
+
+            for t = find(decoding_all.time >= 0 & decoding_all.time <= tpeak(i,peak,s))
+
+                if tc(t) >= 0.5 + (tc(ipeak)-0.5) * peaks(p)
+                    tpeak(i,p,s) = decoding_all.time(t);
+                    break
+                end
+        
+            end 
+
+        end
+
+        i = i+1;
+
+    end
+
+end
+
+mean_tpeak = mean(tpeak,3);
+se_tpeak = std(tpeak,[],3)/sqrt(size(tpeak,3));
+
+%% Plot
+
+figure;
+
+for i = 1:length(peakperc)
+
+    subplot(length(peakperc),1,i)
+
+    bar(mean_tpeak(:,i));
+    hold on;
+    scatter(1:4, squeeze(tpeak(:,i,:)), "filled", 'jitter', 'on')
+    errorbar(mean_tpeak(:,i),se_tpeak(:,i), 'Color', 'black', 'LineWidth', 1.5, 'LineStyle', 'none');
+    
+    
+    ylabel('Peak time (s)'); xlabel('Condition'); title(peakperc{i})
+    set(gca,'XtickLabel', probe_titles);
+
+end
+
+%% Plot peak moment (grand average)
+
+%% On TFR
 
 tpeak = zeros(4,2); i = 1;
 
@@ -127,74 +208,7 @@ for i = 1:length(peakperc)
 
 end
 
-%% Decoding
-
-fn = fieldnames(decoding_all);
-fn = fn(contains(fn, 'correct'));
-fn = fn(~contains(fn, 'tilt'));
-
-time_index     = decoding_all.time >= -100 & decoding_all.time <= 1500;
-
-for f = 1:length(fn)
-    decoding_all.(fn{f}) = decoding_all.(fn{f})(:,time_index);
-end
-
-
-%% Jackknife, leave one out
-
-tpeak = zeros(4,2,25);
-
-for s = 1:25
-    
-    jsubs = subs;
-    jsubs(s) = [];
-    
-    i = 1;
-
-    for f = [1,3,2,4]
-        tc = mean(decoding_all.(fn{f})(jsubs,:),1);
-        tpeak(i,1,s) = decoding_all.time(find(tc == max(tc)));
-        ipeak = find(decoding_all.time == tpeak(i,1,s));
-
-        for t = find(decoding_all.time >= 0 & decoding_all.time <= tpeak(i,1,s))
-
-            if tc(t) >= 0.5 + (tc(ipeak)-0.5) * 0.50
-                tpeak(i,2,s) = decoding_all.time(t);
-                break
-            end
-    
-        end 
-        i = i+1;
-    end
-
-end
-
-mean_tpeak = mean(tpeak,3);
-se_tpeak = std(tpeak,[],3)/sqrt(size(tpeak,3));
-
-%% Plot
-
-ylims = {[0 1500], [0 650]};
-
-figure;
-
-for i = 1:length(peakperc)
-
-    subplot(length(peakperc),1,i)
-
-    bar(mean_tpeak(:,i));
-    hold on;
-    scatter(1:4, squeeze(tpeak(:,i,:)), "filled", 'jitter', 'on')
-    errorbar(mean_tpeak(:,i),se_tpeak(:,i), 'Color', 'black', 'LineWidth', 1.5, 'LineStyle', 'none');
-    
-    
-    ylabel('Peak time (s)'); xlabel('Condition'); title(peakperc{i})
-    set(gca,'XtickLabel', probe_titles);
-    ylim(ylims{i})
-
-end
-
-%% Plot peak moment (grand average)
+%% On decoding
 
 tpeak = zeros(4,2); i = 1;
 
