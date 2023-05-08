e1359d42b1c7e88993710536f5d88b86f3bb9da3
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-05-08 15:36:50 +0200

    Time course plots of performance splits

--- EEG/Probe-locked/TRF performance/rn4_probe_GA_TFR_perf.m
+++ EEG/Probe-locked/TRF performance/rn4_probe_GA_TFR_perf.m
@@ -41,6 +41,144 @@ for f = 1:length(fn)
     mean_cvsi_perf_all.(fn{f}) = reshape(c,s);
 end
 
+
+%% Time-courses
+
+beta_index     = cvsi_perf_all.freq >= param.betaband(1) & cvsi_perf_all.freq <= param.betaband(2);
+alpha_index    = cvsi_perf_all.freq >= param.alphaband(1) & cvsi_perf_all.freq <= param.alphaband(2);
+
+cvsi_perf_all.motor_beta_load_two_fast = squeeze(mean(squeeze(cvsi_perf_all.motor_load_two_fast(:,:,beta_index,:)),2));
+cvsi_perf_all.motor_beta_load_two_slow = squeeze(mean(squeeze(cvsi_perf_all.motor_load_two_slow(:,:,beta_index,:)),2));
+
+cvsi_perf_all.motor_beta_load_four_fast = squeeze(mean(squeeze(cvsi_perf_all.motor_load_four_fast(:,:,beta_index,:)),2));
+cvsi_perf_all.motor_beta_load_four_slow = squeeze(mean(squeeze(cvsi_perf_all.motor_load_four_slow(:,:,beta_index,:)),2));
+
+cvsi_perf_all.motor_beta_load_two_prec = squeeze(mean(squeeze(cvsi_perf_all.motor_load_two_prec(:,:,beta_index,:)),2));
+cvsi_perf_all.motor_beta_load_two_imprec = squeeze(mean(squeeze(cvsi_perf_all.motor_load_two_imprec(:,:,beta_index,:)),2));
+
+cvsi_perf_all.motor_beta_load_four_prec = squeeze(mean(squeeze(cvsi_perf_all.motor_load_four_prec(:,:,beta_index,:)),2));
+cvsi_perf_all.motor_beta_load_four_imprec = squeeze(mean(squeeze(cvsi_perf_all.motor_load_four_imprec(:,:,beta_index,:)),2));
+
+
+cvsi_perf_all.visual_alpha_load_two_fast = squeeze(mean(squeeze(cvsi_perf_all.visual_load_two_fast(:,:,alpha_index,:)),2));
+cvsi_perf_all.visual_alpha_load_two_slow = squeeze(mean(squeeze(cvsi_perf_all.visual_load_two_slow(:,:,alpha_index,:)),2));
+
+cvsi_perf_all.visual_alpha_load_four_fast = squeeze(mean(squeeze(cvsi_perf_all.visual_load_four_fast(:,:,alpha_index,:)),2));
+cvsi_perf_all.visual_alpha_load_four_slow = squeeze(mean(squeeze(cvsi_perf_all.visual_load_four_slow(:,:,alpha_index,:)),2));
+
+cvsi_perf_all.visual_alpha_load_two_prec = squeeze(mean(squeeze(cvsi_perf_all.visual_load_two_prec(:,:,alpha_index,:)),2));
+cvsi_perf_all.visual_alpha_load_two_imprec = squeeze(mean(squeeze(cvsi_perf_all.visual_load_two_imprec(:,:,alpha_index,:)),2));
+
+cvsi_perf_all.visual_alpha_load_four_prec = squeeze(mean(squeeze(cvsi_perf_all.visual_load_four_prec(:,:,alpha_index,:)),2));
+cvsi_perf_all.visual_alpha_load_four_imprec = squeeze(mean(squeeze(cvsi_perf_all.visual_load_four_imprec(:,:,alpha_index,:)),2));
+
+
+%% Plot variables
+
+load_titles = {'Load two', 'Load four'};
+class_titles = {'Motor', 'Visual'};
+
+time = cvsi_perf_all.time;
+
+DT = {'fast', 'slow'};
+ERR = {'prec', 'imprec'};
+LOAD = {'two', 'four'};
+
+%% Plot time-courses
+
+%% Motor: fast vs slow
+
+figure;
+sgtitle('Motor selection: fast vs slow')
+
+for i = 1:length(load_titles)
+
+    subplot(1,2,i)
+
+    fast = cvsi_perf_all.(append('motor_beta_load_', LOAD{i}, '_fast'));
+    slow = cvsi_perf_all.(append('motor_beta_load_', LOAD{i}, '_slow'));
+
+    frevede_errorbarplot(time, fast, param.cols_RGB{1}, 'se');
+    frevede_errorbarplot(time, slow, param.cols_RGB{2}, 'se');
+    
+    xlabel('time (s)'); ylabel('cvsi power change (%)');  
+    title(load_titles{i})
+    xline(0, '--k'); yline(0, '--k')
+    xlim([-0.1 1.5]); 
+    legend('Fast','','Slow','','','')
+
+end
+
+%% Visual: fast vs slow
+
+figure;
+sgtitle('Visual selection: fast vs slow')
+
+for i = 1:length(load_titles)
+
+    subplot(1,2,i)
+
+    fast = cvsi_perf_all.(append('visual_alpha_load_', LOAD{i}, '_fast'));
+    slow = cvsi_perf_all.(append('visual_alpha_load_', LOAD{i}, '_slow'));
+
+    frevede_errorbarplot(time, fast, param.cols_RGB{1}, 'se');
+    frevede_errorbarplot(time, slow, param.cols_RGB{2}, 'se');
+    
+    xlabel('time (s)'); ylabel('cvsi power change (%)');  
+    title(load_titles{i})
+    xline(0, '--k'); yline(0, '--k')
+    xlim([-0.1 1.5]); 
+    legend('Fast','','Slow','','','')
+
+end
+
+%% Motor: prec vs imprec
+
+figure;
+sgtitle('Motor selection: precision')
+
+for i = 1:length(load_titles)
+
+    subplot(1,2,i)
+
+    fast = cvsi_perf_all.(append('motor_beta_load_', LOAD{i}, '_prec'));
+    slow = cvsi_perf_all.(append('motor_beta_load_', LOAD{i}, '_imprec'));
+
+    frevede_errorbarplot(time, fast, param.cols_RGB{1}, 'se');
+    frevede_errorbarplot(time, slow, param.cols_RGB{2}, 'se');
+    
+    xlabel('time (s)'); ylabel('cvsi power change (%)');  
+    title(load_titles{i})
+    xline(0, '--k'); yline(0, '--k')
+    xlim([-0.1 1.5]); 
+    legend('Precise','','Imprecise','','','')
+
+end
+
+%% Visual: prec vs imprec
+
+figure;
+sgtitle('Visual selection: precision')
+
+for i = 1:length(load_titles)
+
+    subplot(1,2,i)
+
+    fast = cvsi_perf_all.(append('visual_alpha_load_', LOAD{i}, '_prec'));
+    slow = cvsi_perf_all.(append('visual_alpha_load_', LOAD{i}, '_imprec'));
+
+    frevede_errorbarplot(time, fast, param.cols_RGB{1}, 'se');
+    frevede_errorbarplot(time, slow, param.cols_RGB{2}, 'se');
+    
+    xlabel('time (s)'); ylabel('cvsi power change (%)');  
+    title(load_titles{i})
+    xline(0, '--k'); yline(0, '--k')
+    xlim([-0.1 1.5]); 
+    legend('Fast','','Slow','','','')
+
+end
+
+
 %% Quick plot TFR
 
 %% Fast vs slow
@@ -75,6 +213,14 @@ plot_TFR_perf(fn, mean_cvsi_perf_all, ...
               'visual', 'prec', 'imprec', ...
               'maxabs', titles)
 
+
+
+
+%% Plot Timecourses
+
+
+
+
 %% TFR plot
 
 function plot_TFR_perf(fn, cvsi, mod, perf1, perf2, zlim, titles)
@@ -106,3 +252,5 @@ function plot_TFR_perf(fn, cvsi, mod, perf1, perf2, zlim, titles)
     end
 
 end
+
+
